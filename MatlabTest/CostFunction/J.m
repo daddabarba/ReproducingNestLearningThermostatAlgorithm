85d94ad09f1130dbaccfed081375e643a0fc6c6b
daddabarba <davidebarbieri97@gmail.com>
2015-09-08 14:03:24 -0700

    adding regularization term

--- MatlabTest/CostFunction/J.m
+++ MatlabTest/CostFunction/J.m
@@ -1,9 +1,9 @@
-function j = J(X , y , theta)
+function j = J(X , y , theta , lambda)
 %the cost function returns the error of 
 %the Hypothetical function
 
 	m = size(X,1);
 
-	j = sum(((X*theta) - y).^2)/(2*m)
+	j = (sum(((X*theta) - y).^2) + lambda*sum(theta(2:end).^2))/(2*m);
 
 end;
